1dde15cbfa0935183446fc1e136b5305d2f325ae
qinlang_ylz <qinlang_ylz@newhope.cn>
2025-01-06 03:47:17 +0000

    优化 对namespace的重复遍历

--- pkg/kube/image.go
+++ pkg/kube/image.go
@@ -69,57 +69,51 @@ func GetWorkloadImage(ctx context.Context, kubeconfig, workload, namespace strin
 			}
 
 		} else if namespace == "all" {
-			nsList, err := client.CoreV1().Namespaces().List(ctx, metav1.ListOptions{})
+			deployItems, err := client.AppsV1().Deployments("").List(ctx, metav1.ListOptions{})
 			if err != nil {
 				klog.Error(ctx, err.Error())
 			}
-			for _, ns := range nsList.Items {
-				deployItems, err := client.AppsV1().Deployments(ns.Name).List(ctx, metav1.ListOptions{})
-				if err != nil {
-					klog.Error(ctx, err.Error())
+			for _, values := range deployItems.Items {
+				for i := 0; i < len(values.Spec.Template.Spec.Containers); i++ {
+					deployMap := make(map[string]string)
+					deployMap["NAMESPACE"] = values.Namespace
+					deployMap["资源类型"] = "deployment"
+					deployMap["资源名"] = values.Name
+					deployMap["镜像地址"] = values.Spec.Template.Spec.Containers[i].Image
+					deployMap["容器名"] = values.Spec.Template.Spec.Containers[i].Name
+					ItemList = append(ItemList, deployMap)
 				}
-				for _, values := range deployItems.Items {
-					for i := 0; i < len(values.Spec.Template.Spec.Containers); i++ {
-						deployMap := make(map[string]string)
-						deployMap["NAMESPACE"] = values.Namespace
-						deployMap["资源类型"] = "deployment"
-						deployMap["资源名"] = values.Name
-						deployMap["镜像地址"] = values.Spec.Template.Spec.Containers[i].Image
-						deployMap["容器名"] = values.Spec.Template.Spec.Containers[i].Name
-						ItemList = append(ItemList, deployMap)
-					}
-				}
+			}
 
-				stsItems, err := client.AppsV1().StatefulSets(ns.Name).List(ctx, metav1.ListOptions{})
-				if err != nil {
-					klog.Error(ctx, err.Error())
+			stsItems, err := client.AppsV1().StatefulSets("").List(ctx, metav1.ListOptions{})
+			if err != nil {
+				klog.Error(ctx, err.Error())
+			}
+			for _, values := range stsItems.Items {
+				for i := 0; i < len(values.Spec.Template.Spec.Containers); i++ {
+					deployMap := make(map[string]string)
+					deployMap["NAMESPACE"] = values.Namespace
+					deployMap["资源类型"] = "statefulsets"
+					deployMap["资源名"] = values.Name
+					deployMap["镜像地址"] = values.Spec.Template.Spec.Containers[i].Image
+					deployMap["容器名"] = values.Spec.Template.Spec.Containers[i].Name
+					ItemList = append(ItemList, deployMap)
 				}
-				for _, values := range stsItems.Items {
-					for i := 0; i < len(values.Spec.Template.Spec.Containers); i++ {
-						deployMap := make(map[string]string)
-						deployMap["NAMESPACE"] = values.Namespace
-						deployMap["资源类型"] = "statefulsets"
-						deployMap["资源名"] = values.Name
-						deployMap["镜像地址"] = values.Spec.Template.Spec.Containers[i].Image
-						deployMap["容器名"] = values.Spec.Template.Spec.Containers[i].Name
-						ItemList = append(ItemList, deployMap)
-					}
-				}
+			}
 
-				dsItems, err := client.AppsV1().DaemonSets(ns.Name).List(ctx, metav1.ListOptions{})
-				if err != nil {
-					klog.Error(ctx, err.Error())
-				}
-				for _, values := range dsItems.Items {
-					for i := 0; i < len(values.Spec.Template.Spec.Containers); i++ {
-						deployMap := make(map[string]string)
-						deployMap["NAMESPACE"] = values.Namespace
-						deployMap["资源类型"] = "daemonsets"
-						deployMap["资源名"] = values.Name
-						deployMap["镜像地址"] = values.Spec.Template.Spec.Containers[i].Image
-						deployMap["容器名"] = values.Spec.Template.Spec.Containers[i].Name
-						ItemList = append(ItemList, deployMap)
-					}
+			dsItems, err := client.AppsV1().DaemonSets("").List(ctx, metav1.ListOptions{})
+			if err != nil {
+				klog.Error(ctx, err.Error())
+			}
+			for _, values := range dsItems.Items {
+				for i := 0; i < len(values.Spec.Template.Spec.Containers); i++ {
+					deployMap := make(map[string]string)
+					deployMap["NAMESPACE"] = values.Namespace
+					deployMap["资源类型"] = "daemonsets"
+					deployMap["资源名"] = values.Name
+					deployMap["镜像地址"] = values.Spec.Template.Spec.Containers[i].Image
+					deployMap["容器名"] = values.Spec.Template.Spec.Containers[i].Name
+					ItemList = append(ItemList, deployMap)
 				}
 			}
 
@@ -143,27 +137,22 @@ func GetWorkloadImage(ctx context.Context, kubeconfig, workload, namespace strin
 				}
 			}
 		} else if namespace == "all" {
-			nsList, err := client.CoreV1().Namespaces().List(ctx, metav1.ListOptions{})
+			items, err := client.AppsV1().Deployments("").List(ctx, metav1.ListOptions{})
 			if err != nil {
 				klog.Error(ctx, err.Error())
 			}
-			for _, ns := range nsList.Items {
-				items, err := client.AppsV1().Deployments(ns.Name).List(ctx, metav1.ListOptions{})
-				if err != nil {
-					klog.Error(ctx, err.Error())
-				}
-				for _, values := range items.Items {
-					for i := 0; i < len(values.Spec.Template.Spec.Containers); i++ {
-						deployMap := make(map[string]string)
-						deployMap["NAMESPACE"] = values.Namespace
-						deployMap["资源类型"] = "deployment"
-						deployMap["资源名"] = values.Name
-						deployMap["镜像地址"] = values.Spec.Template.Spec.Containers[i].Image
-						deployMap["容器名"] = values.Spec.Template.Spec.Containers[i].Name
-						ItemList = append(ItemList, deployMap)
-					}
+			for _, values := range items.Items {
+				for i := 0; i < len(values.Spec.Template.Spec.Containers); i++ {
+					deployMap := make(map[string]string)
+					deployMap["NAMESPACE"] = values.Namespace
+					deployMap["资源类型"] = "deployment"
+					deployMap["资源名"] = values.Name
+					deployMap["镜像地址"] = values.Spec.Template.Spec.Containers[i].Image
+					deployMap["容器名"] = values.Spec.Template.Spec.Containers[i].Name
+					ItemList = append(ItemList, deployMap)
 				}
 			}
+
 		}
 	case "sts", "statefulsets":
 		if namespace != "all" {
@@ -183,27 +172,22 @@ func GetWorkloadImage(ctx context.Context, kubeconfig, workload, namespace strin
 				}
 			}
 		} else if namespace == "all" {
-			nsList, err := client.CoreV1().Namespaces().List(ctx, metav1.ListOptions{})
+			items, err := client.AppsV1().StatefulSets("").List(ctx, metav1.ListOptions{})
 			if err != nil {
 				klog.Error(ctx, err.Error())
 			}
-			for _, ns := range nsList.Items {
-				items, err := client.AppsV1().StatefulSets(ns.Name).List(ctx, metav1.ListOptions{})
-				if err != nil {
-					klog.Error(ctx, err.Error())
-				}
-				for _, values := range items.Items {
-					for i := 0; i < len(values.Spec.Template.Spec.Containers); i++ {
-						deployMap := make(map[string]string)
-						deployMap["NAMESPACE"] = values.Namespace
-						deployMap["资源类型"] = "statefulsets"
-						deployMap["资源名"] = values.Name
-						deployMap["镜像地址"] = values.Spec.Template.Spec.Containers[i].Image
-						deployMap["容器名"] = values.Spec.Template.Spec.Containers[i].Name
-						ItemList = append(ItemList, deployMap)
-					}
+			for _, values := range items.Items {
+				for i := 0; i < len(values.Spec.Template.Spec.Containers); i++ {
+					deployMap := make(map[string]string)
+					deployMap["NAMESPACE"] = values.Namespace
+					deployMap["资源类型"] = "statefulsets"
+					deployMap["资源名"] = values.Name
+					deployMap["镜像地址"] = values.Spec.Template.Spec.Containers[i].Image
+					deployMap["容器名"] = values.Spec.Template.Spec.Containers[i].Name
+					ItemList = append(ItemList, deployMap)
 				}
 			}
+
 		}
 	case "ds", "daemonsets":
 		if namespace != "all" {
@@ -223,27 +207,22 @@ func GetWorkloadImage(ctx context.Context, kubeconfig, workload, namespace strin
 				}
 			}
 		} else if namespace == "all" {
-			nsList, err := client.CoreV1().Namespaces().List(ctx, metav1.ListOptions{})
+			items, err := client.AppsV1().DaemonSets("").List(ctx, metav1.ListOptions{})
 			if err != nil {
 				klog.Error(ctx, err.Error())
 			}
-			for _, ns := range nsList.Items {
-				items, err := client.AppsV1().DaemonSets(ns.Name).List(ctx, metav1.ListOptions{})
-				if err != nil {
-					klog.Error(ctx, err.Error())
-				}
-				for _, values := range items.Items {
-					for i := 0; i < len(values.Spec.Template.Spec.Containers); i++ {
-						deployMap := make(map[string]string)
-						deployMap["NAMESPACE"] = values.Namespace
-						deployMap["资源类型"] = "daemonsets"
-						deployMap["资源名"] = values.Name
-						deployMap["镜像地址"] = values.Spec.Template.Spec.Containers[i].Image
-						deployMap["容器名"] = values.Spec.Template.Spec.Containers[i].Name
-						ItemList = append(ItemList, deployMap)
-					}
+			for _, values := range items.Items {
+				for i := 0; i < len(values.Spec.Template.Spec.Containers); i++ {
+					deployMap := make(map[string]string)
+					deployMap["NAMESPACE"] = values.Namespace
+					deployMap["资源类型"] = "daemonsets"
+					deployMap["资源名"] = values.Name
+					deployMap["镜像地址"] = values.Spec.Template.Spec.Containers[i].Image
+					deployMap["容器名"] = values.Spec.Template.Spec.Containers[i].Name
+					ItemList = append(ItemList, deployMap)
 				}
 			}
+
 		}
 	}
 
--- pkg/kube/resource_test.go
+++
@@ -1,1 +0,0 @@
-package kube_test
