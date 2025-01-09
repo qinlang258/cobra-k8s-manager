0bf6758394bc4baf40eefb5d9c0cf2942d7cc977
qinlang_ylz <qinlang_ylz@newhope.cn>
2025-01-09 02:50:46 +0000

    完善 k8s-manager top 默认输出当前namespace的资源信息

--- pkg/kube/clientset.go
+++ pkg/kube/clientset.go
@@ -2,12 +2,23 @@ package kube
 
 import (
 	"context"
+	"io/ioutil"
+	"regexp"
 
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/clientcmd/api"
 	"k8s.io/klog/v2"
 	"k8s.io/metrics/pkg/client/clientset/versioned"
 )
+
+// 定义 kubeconfig 文件结构体
+type KubeConfig struct {
+	APIVersion     string        `yaml:"apiVersion"`
+	Kind           string        `yaml:"kind"`
+	Contexts       []api.Context `yaml:"contexts"`
+	CurrentContext string        `yaml:"currentContext"`
+}
 
 func NewClientset(configPath string) (*kubernetes.Clientset, error) {
 
@@ -24,6 +35,43 @@ func NewClientset(configPath string) (*kubernetes.Clientset, error) {
 		}
 		return kubernetes.NewForConfig(config)
 	}
+}
+
+func GetClientgoNamespace(configPath string) string {
+	if configPath != "" {
+		bytes, err := ioutil.ReadFile(configPath)
+		if err != nil {
+			klog.Error("没有找到该配置文件", configPath)
+		}
+
+		// 正则表达式查找 namespace 字段
+		re := regexp.MustCompile(`namespace:\s*(\S+)`)
+		matches := re.FindStringSubmatch(string(bytes))
+
+		// 如果找到了匹配项
+		if len(matches) > 1 {
+			return matches[1]
+		} else {
+			return "default"
+		}
+	} else {
+		bytes, err := ioutil.ReadFile(clientcmd.RecommendedHomeFile)
+		if err != nil {
+			klog.Error("没有找到该配置文件", clientcmd.RecommendedHomeFile)
+		}
+
+		// 正则表达式查找 namespace 字段
+		re := regexp.MustCompile(`namespace:\s*(\S+)`)
+		matches := re.FindStringSubmatch(string(bytes))
+
+		// 如果找到了匹配项
+		if len(matches) > 1 {
+			return matches[1]
+		} else {
+			return "default"
+		}
+	}
+
 }
 
 func NewMetricsClient(configPath string) (*versioned.Clientset, error) {
--- pkg/kube/top.go
+++ pkg/kube/top.go
@@ -60,8 +60,10 @@ func GetPodTopInfoWithNamespaceAndNode(ctx context.Context, kubeconfig, workload
 			ItemList = append(ItemList, deployMap)
 
 		}
-		mtable.TablePrint("top", ItemList)
 	}
+
+	mtable.TablePrint("top", ItemList)
+
 }
 
 func GetPodTopInfoWithNode(ctx context.Context, kubeconfig, workload, node string) {
@@ -154,6 +156,120 @@ func GetPodTopInfoWithNode(ctx context.Context, kubeconfig, workload, node strin
 				}
 			}
 
+		}
+	}
+
+	mtable.TablePrint("top", ItemList)
+}
+
+func GetPodTopInfoWithCurrentNamespace(ctx context.Context, kubeconfig string) {
+	client, err := NewClientset(kubeconfig)
+	if err != nil {
+		klog.Error(ctx, err.Error())
+		return
+	}
+
+	metricsClient, err := NewMetricsClient(kubeconfig)
+	if err != nil {
+		klog.Error(ctx, err.Error())
+		return
+	}
+
+	ItemList := make([]map[string]string, 0)
+	namespace := GetClientgoNamespace(kubeconfig)
+
+	podListItem, err := client.CoreV1().Pods(namespace).List(ctx, metav1.ListOptions{})
+	if err != nil {
+		klog.Error(ctx, err.Error())
+	}
+
+	for _, pod := range podListItem.Items {
+		deployMap := make(map[string]string)
+		deployMap["节点名"] = pod.Spec.NodeName
+		deployMap["NAMESPACE"] = pod.Namespace
+		deployMap["资源类型"] = pod.OwnerReferences[0].Kind
+		deployMap["资源名"] = pod.OwnerReferences[0].Name
+		deployMap["POD_NAME"] = pod.Name
+
+		podMetrics, err := metricsClient.MetricsV1beta1().PodMetricses(pod.Namespace).Get(ctx, pod.Name, metav1.GetOptions{})
+		if err != nil {
+			klog.Error(ctx, "Error fetching metrics for pod ", pod.Name, ": ", err.Error())
+			continue
+		}
+
+		// 获取 CPU 和内存使用数据
+		for i := 0; i < len(podMetrics.Containers); i++ {
+			cpuUsage := podMetrics.Containers[i].Usage.Cpu()
+			memoryUsage := podMetrics.Containers[i].Usage.Memory()
+			usedMemoryMi := float64(memoryUsage.Value()) / 1024 / 1024
+			usedCpuCores := float64(cpuUsage.MilliValue())
+
+			deployMap["当前已使用的CPU"] = fmt.Sprintf("%.2fm", usedCpuCores)
+			deployMap["当前已使用的内存"] = fmt.Sprintf("%.2fm", usedMemoryMi)
+		}
+
+		ItemList = append(ItemList, deployMap)
+	}
+
+	mtable.TablePrint("top", ItemList)
+
+}
+
+func GetPodAllTopInfo(ctx context.Context, kubeconfig string) {
+	client, err := NewClientset(kubeconfig)
+	if err != nil {
+		klog.Error(ctx, err.Error())
+		return
+	}
+
+	metricsClient, err := NewMetricsClient(kubeconfig)
+	if err != nil {
+		klog.Error(ctx, err.Error())
+		return
+	}
+
+	ItemList := make([]map[string]string, 0)
+
+	podListItem, err := client.CoreV1().Pods("").List(ctx, metav1.ListOptions{})
+	if err != nil {
+		klog.Error(ctx, err.Error())
+	}
+
+	nodeListItem, err := client.CoreV1().Nodes().List(ctx, metav1.ListOptions{})
+	if err != nil {
+		klog.Error(ctx, err.Error())
+	}
+
+	for _, node := range nodeListItem.Items {
+		for _, pod := range podListItem.Items {
+			if node.Name == pod.Spec.NodeName {
+				deployMap := make(map[string]string)
+				deployMap["节点名"] = pod.Spec.NodeName
+				deployMap["NAMESPACE"] = pod.Namespace
+				deployMap["资源类型"] = pod.OwnerReferences[0].Kind
+				deployMap["资源名"] = pod.OwnerReferences[0].Name
+				deployMap["POD_NAME"] = pod.Name
+
+				podMetrics, err := metricsClient.MetricsV1beta1().PodMetricses(pod.Namespace).Get(ctx, pod.Name, metav1.GetOptions{})
+				if err != nil {
+					klog.Error(ctx, "Error fetching metrics for pod ", pod.Name, ": ", err.Error())
+					continue
+				}
+
+				// 获取 CPU 和内存使用数据
+				for i := 0; i < len(podMetrics.Containers); i++ {
+					cpuUsage := podMetrics.Containers[i].Usage.Cpu()
+					memoryUsage := podMetrics.Containers[i].Usage.Memory()
+					usedMemoryMi := float64(memoryUsage.Value()) / 1024 / 1024
+					usedCpuCores := float64(cpuUsage.MilliValue())
+
+					deployMap["当前已使用的CPU"] = fmt.Sprintf("%.2fm", usedCpuCores)
+					deployMap["当前已使用的内存"] = fmt.Sprintf("%.2fm", usedMemoryMi)
+				}
+
+				ItemList = append(ItemList, deployMap)
+
+			}
 		}
 	}
 
